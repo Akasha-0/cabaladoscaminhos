70e4597648b6bbc69fba4bc41eb10d1de808e650
Akasha-0 <akasha.arth@gmail.com>
2026-05-29 10:24:08 -0300

    cleanup

--- tests/lib/correlation/deep-engine.test.ts
+++
@@ -1,5 +0,0 @@
-import { describe, test, expect } from 'vitest';
-import { getDayCorrelations } from '../../../src/lib/correlation/deep-engine';
-describe('Deep', () => {
-  test('basic', () => { expect(getDayCorrelations('segunda-feira')?.orixas).toContain('Omolu'); });
-});
