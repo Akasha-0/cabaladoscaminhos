b1109011ceb48a6762256706d3d06d1873fa44d5
Akasha-0 <akasha.arth@gmail.com>
2026-05-31 02:58:00 -0300

    chore: cleanup responsive section - cycle 627

---
+++ src/components/design-system/index.ts
@@ -0,0 +1,9 @@
+// Design System Components - Mystical UI
+export { ThemeProvider } from './ThemeProvider';
+export { ThemeToggle } from './ThemeToggle';
+export { ThemeSwitcher } from './ThemeSwitcher';
+export { GlowEffect } from './GlowEffect';
+export { CosmicBackground } from './CosmicBackground';
+export { Typography, Heading, Text } from './Typography';
+export { MobileTooltip, TooltipContent } from './MobileTooltip';
+export { ResponsiveSectionCard, ResponsiveGlowCard } from './ResponsiveSectionCard';
